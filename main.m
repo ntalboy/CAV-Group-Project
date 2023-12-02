706d7a0beb22c0f4efce87b2719daac654338485
Jacob Johnson <bocajajohnson@gmail.com>
2023-12-02 12:15:16 -0500

    rename scenario_1_main to main, fix indexing bug

---
+++ main.m
@@ -0,0 +1,56 @@
+function main
+clc
+close all
+[allData, scenario, sensors] = scenario_1();
+plot(scenario)
+
+% start at 2 because index 1 time is zero, and simulation starts at 0.01
+detectionsIndex = 1;
+numDetections = length(allData);
+
+while advance(scenario)
+    pause(0.05)
+    
+    % fprintf('simulation time: %.2f\n',scenario.SimulationTime)
+    % edge case when goes to last index
+    if numDetections > detectionsIndex
+        ObjectDetections = allData(detectionsIndex).ObjectDetections;
+        numObjects = length(ObjectDetections);
+    else
+        % set back to the beginning so that it won't run anymore
+        % disp('continuing')
+        continue
+    end
+    
+    if numObjects > 0
+        if ObjectDetections{1}.Time <= scenario.SimulationTime
+            detectionsIndex = detectionsIndex + 1;
+            % fprintf('number of detections: %d\n',numObjects)
+            
+    
+            
+            for i = 1:numObjects
+                xmeasurement = ObjectDetections{i}.Measurement(1);
+                ymeasurement = ObjectDetections{i}.Measurement(2);
+                distance = sqrt(xmeasurement^2 + ymeasurement^2);
+                % disp(measurement)
+                % disp(distance)
+                if distance < 4
+                    if abs(xmeasurement) > abs(ymeasurement)
+                        if xmeasurement > 0
+                            disp('too close in front!')
+                        else
+                            disp('too close in back!')
+                        end
+                    else
+                        if ymeasurement > 0
+                            disp('too close on left!')
+                        else
+                            disp('too close on right!')
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
